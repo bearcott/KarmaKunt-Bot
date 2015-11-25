430df571cc8b08809abf8784833e6e74d627717a
bearcott <bear@bearcott.com>
2015-11-25 00:24:44 -0600

    parse comments for mentioning /r/subreddit

--- __main__.py
+++ __main__.py
@@ -1,11 +1,16 @@
 import time
 import praw
+from pprint import pprint
 
-r = praw.Reddit('KarmaKunt Bot.')
-r.login()
+user_agent = 'KarmaKunt Karma nabbing machine 1.0 by /u/sausagefest2011'
+r = praw.Reddit(user_agent=user_agent)
 
-
-
-
-
+#main running loop
+#while True:
+sub = r.get_subreddit("videos")
+comments = praw.helpers.flatten_tree(sub.get_comments())
+for comment in comments:
+    results = [word for word in comment.body.split() if word.startswith('/r/')]
+    pprint(results)    
+    #time.sleep(10)
 
