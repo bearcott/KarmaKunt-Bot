b0c8178a14e019cd282d4d2643ff206158a75835
bearcott <bear@bearcott.com>
2015-11-28 02:03:56 -0600

    working on oauth

--- __main__.py
+++ __main__.py
@@ -2,9 +2,20 @@ import time
 import praw
 import regex as re
 from pprint import pprint
+import config
+import webbrowser
 
 user_agent = 'KarmaKunt Karma nabbing machine 1.0 by /u/sausagefest2011'
 r = praw.Reddit(user_agent=user_agent)
+
+#set up Oauth for account
+r.set_oauth_app_info(client_id=config.ID,
+                     client_secret=config.SECRET,
+                     redirect_uri='http://127.0.0.1:65010/authorize_callback')
+
+url = r.get_authorize_url('RandDdoMaskShitStrkingFurU.Haxkor!', 'identity', True)
+webbrowser.open(url)
+
 
 #main running loop
 #while True:
@@ -12,12 +23,12 @@ subname = u"videos"
 sub = r.get_subreddit(subname)
 comments = sub.get_comments(limit=None,threshold=2)
 count = 0
+
 for comment in comments:
     if comment.score >= 0: #make sure they will like you
         if comment.is_root: #make sure its a top level comment
             #then filter out mentions of subreddits in each comment
             results = [re.sub(ur"\p{P}+", "",word.replace('/r/','')) for word in comment.body.split() if word.startswith('/r/')]
-            print results
             count+=1
             if results: #make sure the sub exists
                 if subname not in results: #make sure it isn't the same sub
